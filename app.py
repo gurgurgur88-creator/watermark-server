606ac18c4e36d19bb3f4828b2a77a933c9920eda
gurgurgur88-creator <gurgurgur88@gmail.com>
2026-01-19 13:56:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -214,17 +214,18 @@ def api_embed():
         _, buf = cv2.imencode(".png", out_bgr)
         b64_str = base64.b64encode(buf).decode("utf-8")
 
-    return jsonify({
-      "ok": True,
-      "template_id": tpl_id,
-      "wm_id": wm_id,
-      "image_base64": b64_str,
-      "image_mime": "image/png"
-    })
+         return jsonify({
+          "ok": True,
+          "template_id": tpl_id,
+          "wm_id": wm_id,
+          "image_base64": b64_str,
+          "image_mime": "image/png"
+        })
     except Exception as e:
         return jsonify({"ok": False, "reason": str(e)}), 500
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
 
 
